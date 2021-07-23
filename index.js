0f4ccd1a22a5796bac7d8eecf8e1354170b37ba6
mhshuvoit <mhshuvoit@gmail.com>
2021-07-23 23:00:10 +0600

    first commit for backend

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const nodemailer = require("nodemailer");
+
+app.use(express.json())
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD
+    }
+});
+
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('This is a simple app for sending email')
+})
+
+app.post('/send', (req, res) => {
+    const { name, email, message } = req.body
+
+    const messageHtml = `
+    <div>
+    <h3>Name: ${name}</h3>
+    <p>Email: ${email}</p>
+    <p>Message: ${message}</p>
+    </div>`
+
+    transporter.sendMail({
+        from: process.env.GMAIL_USER,
+        to: process.env.ADMIT_USER,
+        subject: "Message from your portfolio",
+        html: messageHtml,
+    })
+        .then(() => {
+            res.send('Message sent to admin email')
+        })
+        .catch(() => {
+            res.send('Something is wrong');
+        })
+})
+
+
+app.listen(port, () => {
+    console.log(`App listening at ${port}`)
+})
