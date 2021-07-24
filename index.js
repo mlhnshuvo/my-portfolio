bb1621736894fd1365a324dc159dbcf794c2419f
mhshuvoit <mhshuvoit@gmail.com>
2021-07-24 09:47:31 +0600

    cors add

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const nodemailer = require("nodemailer");
+var cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
