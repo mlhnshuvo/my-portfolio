e6102ac4805d1f7a1838b328ece44d85427b9f40
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-09-12 12:24:42 +0600

    pokemon add

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,61 +1,76 @@
-import React, { useState, useRef } from 'react';
-import projects from '../projectsData'
+import React, { useState, useRef } from "react";
+import projects from "../projectsData";
 
 const Projects = ({ goTo }) => {
-    const [state, setState] = useState(projects)
-    const projectRef = useRef(null)
+  const [state, setState] = useState({
+    projects: projects.filter((project) => project.type === "react"),
+  });
+  const projectRef = useRef(null);
 
-    if (goTo) {
-        projectRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+  if (goTo) {
+    projectRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
+  }
+
+  const handler = (value) => {
+    if (value === "react") {
+      setState({
+        projects: projects.filter((project) => project.type === value),
+      });
+    } else if (value === "mern") {
+      setState({
+        projects: projects.filter((project) => project.type === value),
+      });
+    } else if (value === "others") {
+      setState({
+        projects: projects.filter((project) => project.type === value),
+      });
     }
+  };
 
-    const handler = (e) => {
-        if (e.target.value === 'recent') {
-            const project = projects.filter(project => project.type === e.target.value)
-            setState(project)
-        } else if (e.target.value === 'running') {
-            const project = projects.filter(project => project.type === e.target.value)
-            setState(project)
-        } else {
-            setState(projects)
-        }
-    }
-
-    return (
-        <div className="project section-margin" ref={projectRef}>
-            <h2>Projects</h2>
-            <p>My all own and contribution projects are here</p>
-            <hr />
-            <button
-                className="my-btn btn--project"
-                onClick={handler}
-            >All</button>
-            <button
-                className="my-btn btn--project"
-                onClick={handler}
-                value="recent">Recent</button>
-            <button
-                className="my-btn btn--project"
-                onClick={handler}
-                value="running">Running</button>
-            <div className="project__flex container">
-                {state.map(project => (
-                    <div key={project.live} className="card" data-aos="zoom-in">
-                        <img className="project__image" src={project.image} alt="" />
-                        <p className="mt-3">{project.name}</p>
-                        <div className="project__btn">
-                            <a target="_blank" rel="noopener noreferrer" href={project.github}>
-                                <button className="my-btn btn--project">Github</button>
-                            </a>
-                            <a target="_blank" rel="noopener noreferrer" href={project.live}>
-                                <button className="my-btn btn--project">Live</button>
-                            </a>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <div className="project section-margin" ref={projectRef}>
+      <h2>Projects</h2>
+      <p>My all own and contribution projects are here</p>
+      <hr />
+      <button className="my-btn btn--project" onClick={() => handler("react")}>
+        react
+      </button>
+      <button
+        className="my-btn btn--project"
+        onClick={() => handler("mern")}
+        value="mern"
+      >
+        mern
+      </button>
+      <button
+        className="my-btn btn--project"
+        onClick={() => handler("others")}
+        value="others"
+      >
+        others
+      </button>
+      <div className="project__flex container">
+        {state.projects.map((project) => (
+          <div key={project.live} className="card" data-aos="zoom-in">
+            <img className="project__image" src={project.image} alt="" />
+            <p className="mt-3">{project.name}</p>
+            <div className="project__btn">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={project.github}
+              >
+                <button className="my-btn btn--project">Github</button>
+              </a>
+              <a target="_blank" rel="noopener noreferrer" href={project.live}>
+                <button className="my-btn btn--project">Live</button>
+              </a>
             </div>
-        </div>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
--- src/projectsData.js
+++ src/projectsData.js
@@ -9,74 +9,90 @@ import LandingPage from './assets/images/projects/landing-page.jpg'
 import ReactBlogApp from './assets/images/projects/react-blog-app.jpg'
 import WeatherApp from './assets/images/projects/js-weather-app-with-dom.jpg'
 import EssayContest from './assets/images/projects/essay-contest-website.jpg'
+import PokemonApp from './assets/images/projects/pokemon-app.png'
 
 const projects = [
     {
         image: ShoppingList,
         name: 'This is a reactjs shopping list app inspired by google shopping list app',
-        type: 'recent',
+        type: 'react',
         github: 'https://github.com/mhshuvoit/shopping-list-app',
         live: 'https://shopping-list-app-mhs.netlify.app'
     },
     {
         image: FaceBookLite,
         name: 'This is a mern stack app inspired by facebook',
-        type: 'running',
+        type: 'mern',
         github: 'https://github.com/mhshuvoit/facebook-lite-app',
         live: 'https://facebook-lite-app-mhs.herokuapp.com'
     },
     {
         image: ShoppingCard,
         name: 'This is a shopping cart application with react',
+        type: 'react',
         github: 'https://github.com/mhshuvoit/react-shopping-cart-app',
         live: 'https://react-shopping-cart-mhs.netlify.app'
     },
     {
         image: EssayContest,
         name: 'This is an essay contest website that was created for a client',
-        type: 'recent',
+        type: 'others',
         github: 'https://github.com/mhshuvoit/essay-contest-website',
         live: 'https://essay-contest-website-mhs.netlify.app'
     },
     {
         image: PollAll,
         name: 'This is a simple poll app with react core features',
+        type: 'react',
         github: 'https://github.com/mhshuvoit/react-poll-app',
         live: 'https://react-poll-app-mhs.netlify.app'
     },
     {
+        image: PokemonApp,
+        name: 'This is a demo app using third party api called pokemon',
+        type: 'react',
+        github: 'https://github.com/mlhnshuvo/pokemon-app',
+        live: 'https://pokemon-app-mhs.netlify.app'
+    },
+    {
         image: TodoApp,
         name: 'This is a todo app with react core features',
+        type: 'react',
         github: 'https://github.com/mhshuvoit/react-todo-app',
         live: 'https://react-todo-app-mhs.netlify.app'
     },
     {
         image: ReactBlogApp,
         name: 'This is react blog app with third party API.',
+        type: 'react',
         github: 'https://github.com/mhshuvoit/blog-app',
         live: 'https://blog-app-mhs.netlify.app'
     },
     {
         image: WeatherApp,
         name: 'This is a simple weather app with DOM',
+        type: 'others',
         github: 'https://github.com/mhshuvoit/js-weather-app-with-dom',
         live: 'https://js-weather-app-with-dom-mhs.herokuapp.com'
     },
     {
         image: MyStaticPortfolio,
         name: 'This is first portfolio with HTML, CSS',
+        type: 'others',
         github: 'https://github.com/mhshuvoit/static-portfolio',
         live: 'https://static-portfolio-mhs.netlify.app'
     },
     {
         image: PstToHtml,
         name: 'PSD to HTML landing  page',
+        type: 'others',
         github: 'https://github.com/mhshuvoit/psd-to-html',
         live: 'https://psd-to-html-mhs.netlify.app'
     },
     {
         image: LandingPage,
         name: 'A simple landing page with html, css and totally responsive',
+        type: 'others',
         github: 'https://github.com/mhshuvoit/landing-page',
         live: 'https://landing-page-mhs.netlify.app'
     }
