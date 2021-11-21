30902a8f7c0e5283a08bc3a5898cda59e4b09e16
mlhnshuvo <mlhnshuvo@gmail.com>
2021-11-21 07:02:33 +0600

    trying to fix problem

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
 const nodemailer = require("nodemailer");
-var cors = require('cors')
+const cors = require("cors");
 
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD
-    }
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASSWORD,
+  },
 });
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.post('/send', (req, res) => {
-    const { name, email, message } = req.body
-
-    const messageHtml = `
+app.post("/send", (req, res) => {
+  const { name, email, message } = req.body;
+  let messageHtml = `
     <div>
-    <h3>Name: ${name}</h3>
-    <p>Email: ${email}</p>
-    <p>Message: ${message}</p>
-    </div>`
-
-    transporter.sendMail({
-        from: process.env.GMAIL_USER,
-        to: process.env.ADMIT_USER,
-        subject: "Message from your portfolio",
-        html: messageHtml,  
+        <h3>Name: ${name}</h3>
+        <p>Email: ${email}</p>
+        <p>Message: ${message}</p>
+    </div>`;
+  transporter
+    .sendMail({
+      from: process.env.GMAIL_USER,
+      to: process.env.ADMIT_USER,
+      subject: "Message from your portfolio",
+      html: messageHtml,
     })
-        .then(() => {
-            res.send('Message sent to admin email')
-        })
-        .catch(() => {
-            res.send('Something is wrong');
-        })
-})
+    .then(() => {
+      res.send("Message sent to admin email");
+    })
+    .catch(() => {
+      res.send("Something is wrong");
+    });
+});
 
-app.get('/', (req, res) => {
-    res.send('This is a simple app for sending email')
-})
+app.get("/", (req, res) => {
+  res.send("This is a simple app for sending email");
+});
 
 app.listen(port, () => {
-    console.log(`App listening at ${port}`)
-})
+  console.log(`App listening at ${port}`);
+});
